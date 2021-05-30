0d37aef48b49b084fb92718777cfb129dabe3e9e
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-05-30 10:46:22 +0800

    fetchData function inside useEffect

--- src/api.js
+++ src/api.js
@@ -5,8 +5,12 @@ import { useEffect } from 'react';
 export const useGetUsers = async (url, setUsers) => {
 
   useEffect(() => {
-    axios.get(url).then(
-      (res) => setUsers(res.data))
+    const fetchData = async () => {
+      const res = await axios.get(url)
+      setUsers(res.data)
+    }
+    fetchData()
+    
   },[url, setUsers])
   
 }
